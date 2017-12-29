9b66b4f477c3a51b43f88c3e9ec4288ae4ab641e
karlozz157 <karlozz157@gmail.com>
2017-12-29 10:35:51 -0600

    Added forms

---
+++ src/MyFinancesBundle/Form/IncomeType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MyFinancesBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IncomeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', 'text')
+            ->add('concept')
+            ->add('unity')
+            ->add('price')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false, 
+            'data_class' => \MyFinancesBundle\Entity\Income::class
+        ]);
+    }
+}
